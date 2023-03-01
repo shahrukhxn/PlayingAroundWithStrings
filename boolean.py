59d09f6862b66be0d28ecfb885890346333cf78b
sharukhhxn <sharukhhxn@gmail.com>
2023-03-01 21:43:48 +0500

    understanding Boolean

---
+++ boolean.py
@@ -0,0 +1,10 @@
+character_name = "Dog"
+character_age = 10.5
+is_Male = True
+print("Once upon a time there was a " + character_name +", ")
+print("He was very thirsty and near to die")
+print("so he decided to fly and find water")
+print("and the " + character_name + " ran to many places but all in vain")
+print("at last he found a pitcher of water in the fores")
+print("when he came near the pitcher")
+print("he saw that the water level is too low")
